96be1aaf576baf7e1af24dd5eaa5fda8fb14f5c9
stnava <stnava@gmail.com>
2024-12-06 12:42:18 -0500

    ENH: fs example

--- tests/example_fs.py
+++ tests/example_fs.py
@@ -11,7 +11,7 @@ ctype='fs'
 tcaudR=curvanato.load_labeled_caudate( option='hmt', binarize=False, label=[2,4,6] )
 caudseg=curvanato.load_labeled_caudate( option=ctype )
 # this step is important - fs caudate segmentations look pretty terrible in comparison to what i am used to ....
-caudseg=ants.threshold_image( caudseg, 50, 50 ).resample_image( [0.25,0.25,0.25], interp_type=0 ).threshold_image(0.5,1) 
+caudseg=ants.threshold_image( caudseg, 50, 50 ).resample_image( [0.5,0.5,0.5], interp_type=0 ).threshold_image(0.5,1)
 fn='/tmp/cc_example.nii.gz'
 vlab=None
 gr=0
@@ -28,26 +28,7 @@ plabs=[4]
 mytl=1
 xx = curvanato.t1w_caudcurv(  caudseg, target_label=mytl, ventricle_label=vlab, 
         prior_labels=pcaud, prior_target_label=plabs, subdivide=subd, grid=gr,
-        priorparcellation=tcaudR,  plot=True,
-        verbose=True )
-ants.plot( xx[0], xx[1], crop=True, axis=2, nslices=21, ncol=7, filename=ccfn[4] )
-ants.plot( xx[0], xx[2], crop=True, axis=2, nslices=21, ncol=7, filename=ccfn[5] )
-for j in range(3):
-    ants.image_write( xx[j], ccfn[j] )
-xx[3].to_csv( ccfn[3] )
-##
-##
-##
-ccfn = [
-        re.sub( ".nii.gz", "_"+ctype+"RkappaSmoother.nii.gz" , fn ), 
-        re.sub( ".nii.gz", "_"+ctype+"RSmoother.nii.gz" , fn ),
-        re.sub( ".nii.gz", "_"+ctype+"RthkSmoother.nii.gz" , fn ),
-        re.sub( ".nii.gz", "_"+ctype+"RkappaSmoother.csv" , fn ),
-        re.sub( ".nii.gz", "_"+ctype+"RkappaSmoother.png" , fn ),
-        re.sub( ".nii.gz", "_"+ctype+"RthkSmoother.png" , fn ) ]
-xx = curvanato.t1w_caudcurv(  caudseg, target_label=mytl, ventricle_label=vlab, 
-        prior_labels=pcaud, prior_target_label=plabs, subdivide=subd, grid=gr,
-        priorparcellation=tcaudR,  plot=False, smoothing=2.4, # double
+        priorparcellation=tcaudR,  plot=True, smoothing=0.5,
         verbose=True )
 ants.plot( xx[0], xx[1], crop=True, axis=2, nslices=21, ncol=7, filename=ccfn[4] )
 ants.plot( xx[0], xx[2], crop=True, axis=2, nslices=21, ncol=7, filename=ccfn[5] )
